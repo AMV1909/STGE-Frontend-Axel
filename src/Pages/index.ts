e3ae539c797d1ecc819ef222993d54593c2a2efd
AMV1909 <amorales264@unab.edu.co>
2023-11-30 15:22:38 -0500

    Se volvió a agregar el lazy loading

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,8 +1,47 @@
-export { Home } from "./Home/Home";
-export { Login } from "./Login/Login";
-export { Register } from "./Register/Register";
-export { SelectCoursesToTeach } from "./SelectCoursesToTeach/SelectCoursesToTeach";
-export { Profile } from "./Profile/Profile";
-export { ListTutors } from "./ListTutors/ListTutors";
-export { Admin } from "./Admin/Admin";
-export { NotFound } from "./NotFound/NotFound";
+import { lazy } from "react";
+
+const routes = {
+    Home: lazy(() =>
+        import("./Home/Home").then(({ Home }) => ({ default: Home }))
+    ),
+    Login: lazy(() =>
+        import("./Login/Login").then(({ Login }) => ({ default: Login }))
+    ),
+    Register: lazy(() =>
+        import("./Register/Register").then(({ Register }) => ({
+            default: Register,
+        }))
+    ),
+    SelectCoursesToTeach: lazy(() =>
+        import("./SelectCoursesToTeach/SelectCoursesToTeach").then(
+            ({ SelectCoursesToTeach }) => ({ default: SelectCoursesToTeach })
+        )
+    ),
+    Profile: lazy(() =>
+        import("./Profile/Profile").then(({ Profile }) => ({
+            default: Profile,
+        }))
+    ),
+    ListTutors: lazy(() =>
+        import("./ListTutors/ListTutors").then(({ ListTutors }) => ({
+            default: ListTutors,
+        }))
+    ),
+    Admin: lazy(() =>
+        import("./Admin/Admin").then(({ Admin }) => ({ default: Admin }))
+    ),
+    NotFound: lazy(() =>
+        import("./NotFound/NotFound").then(({ NotFound }) => ({
+            default: NotFound,
+        }))
+    ),
+};
+
+export const Home = routes.Home;
+export const Login = routes.Login;
+export const Register = routes.Register;
+export const SelectCoursesToTeach = routes.SelectCoursesToTeach;
+export const Profile = routes.Profile;
+export const ListTutors = routes.ListTutors;
+export const Admin = routes.Admin;
+export const NotFound = routes.NotFound;
